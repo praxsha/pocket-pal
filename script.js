079302630ba4544d284b556688f39e9c3241f082
praxsha <pratikshathapa2211@gmail.com>
2026-07-25 12:04:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function feedPet() {
+    document.getElementById("dialogue").textContent =
+        "🍎 Yum! That was delicious! Thank you! ❤️";
+}
+
+function playPet() {
+    document.getElementById("dialogue").textContent =
+        "🎾 Yay! Playing with you is my favorite thing! 😸";
+}
+
+function petPet() {
+    document.getElementById("dialogue").textContent =
+        "🥰 Purr... I love head pats! ❤️";
+}
