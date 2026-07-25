cf2d1ccec500a08a9bd4fce90b5f3735c73df9dc
praxsha <pratikshathapa2211@gmail.com>
2026-07-25 12:04:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+let friendship = 0;
+
+function updateFriendship() {
+    document.getElementById("friendship").textContent = friendship;
+}
+
 function feedPet() {
+    friendship += 5;
     document.getElementById("dialogue").textContent =
-        "🍎 Yum! That was delicious! Thank you! ❤️";
+        "🍎 Yum! Thank you! You're the best!";
+    updateFriendship();
 }
 
 function playPet() {
+    friendship += 10;
     document.getElementById("dialogue").textContent =
-        "🎾 Yay! Playing with you is my favorite thing! 😸";
+        "🎾 Yay! That was so much fun!";
+    updateFriendship();
 }
 
 function petPet() {
+    friendship += 3;
     document.getElementById("dialogue").textContent =
-        "🥰 Purr... I love head pats! ❤️";
+        "🥰 Purr... I love cuddles!";
+    updateFriendship();
 }
+
+updateFriendship();
