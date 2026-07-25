cb88d99e27690fd767a30cc623a7a9da0d3d6386
praxsha <pratikshathapa2211@gmail.com>
2026-07-25 12:07:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,85 @@
 let friendship = 0;
 
-function updateFriendship() {
-    document.getElementById("friendship").textContent = friendship;
+const dialogue = document.getElementById("dialogue");
+const friendshipText = document.getElementById("friendship");
+
+const container = document.querySelector(".container");
+
+const progressHTML = `
+<p style="margin-top:15px;">Friendship Progress</p>
+
+<div class="progress">
+    <div class="progress-bar" id="progressBar"></div>
+</div>
+
+<h3 id="rank">🌱 Stranger</h3>
+`;
+
+container.insertAdjacentHTML("beforeend", progressHTML);
+
+const progressBar = document.getElementById("progressBar");
+const rank = document.getElementById("rank");
+
+function updateUI(){
+
+    friendshipText.textContent = friendship;
+
+    let percent = friendship;
+
+    if(percent>100){
+        percent=100;
+    }
+
+    progressBar.style.width = percent + "%";
+
+    if(friendship<25){
+
+        rank.textContent="🌱 Stranger";
+
+    }else if(friendship<50){
+
+        rank.textContent="😊 Friend";
+
+    }else if(friendship<75){
+
+        rank.textContent="💖 Best Friend";
+
+    }else{
+
+        rank.textContent="💍 Soulmate";
+
+    }
+
 }
 
-function feedPet() {
+function feedPet(){
+
     friendship += 5;
-    document.getElementById("dialogue").textContent =
-        "🍎 Yum! Thank you! You're the best!";
-    updateFriendship();
+
+    dialogue.textContent="🍎 Mochi enjoyed the food!";
+
+    updateUI();
+
 }
 
-function playPet() {
+function playPet(){
+
     friendship += 10;
-    document.getElementById("dialogue").textContent =
-        "🎾 Yay! That was so much fun!";
-    updateFriendship();
+
+    dialogue.textContent="🎾 Mochi is having lots of fun!";
+
+    updateUI();
+
 }
 
-function petPet() {
+function petPet(){
+
     friendship += 3;
-    document.getElementById("dialogue").textContent =
-        "🥰 Purr... I love cuddles!";
-    updateFriendship();
+
+    dialogue.textContent="🥰 Purr... Mochi loves your cuddles!";
+
+    updateUI();
+
 }
 
-updateFriendship();
+updateUI();
